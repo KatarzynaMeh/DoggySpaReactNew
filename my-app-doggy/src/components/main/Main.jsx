7d082b1b3e5091effa9b2f27910132fbbce41806
Mekhova Kateryna <katrindeutschland33333@gmail.com>
2024-01-17 20:42:16 +0100

    Finnish Main

--- my-app-doggy/src/components/main/Main.jsx
+++ my-app-doggy/src/components/main/Main.jsx
@@ -3,19 +3,24 @@ import imgDog from './images/big_img_dog.png'
 
 export const Main = () => {
     return (
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", position: 'relative'}}>
         <div className={classes.divContainerMain}></div>
         <div>
-          <div style={{marginBottom: '40px'}}>
-            <p className={classes.paragOne}>Your dog running </p>
-            <p className={classes.paragOne}>amok in the dirt?</p>
+          <div>
+            <p
+              style={{ top: "120px", left: "100px" }}
+              className={classes.paragOne}
+            >
+              Your dog running 
+            </p>
+            <p style={{top: '220px', left: '110px'}} className={classes.paragOne}>amok in the dirt?</p>
           </div>
-          <div style={{marginBottom: '70px'}}>
-            <p className={classes.paragTwo}>Book your doggy </p>
-            <p className={classes.paragTwo}>spa day!</p>
+          <div>
+            <p style={{top: '360px', left: '130px'}} className={classes.paragTwo}>Book your doggy </p>
+            <p style={{top: '460px', left: '220px'}} className={classes.paragTwo}>spa day!</p>
           </div>
 
-          <button className={classes.buttonBook}>Book Appointment</button>
+          <button style={{bottom: '130px', left: '210px'}} className={classes.buttonBook}>Book Appointment</button>
         </div>
         <div>
           <img src={imgDog} alt="Dog" />
